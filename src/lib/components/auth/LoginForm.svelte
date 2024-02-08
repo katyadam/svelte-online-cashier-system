6c7ccc260c4de77e249b65a555ead374f2f33f45
Adam Kattan <kattanadam7@gmail.com>
2024-02-08 20:53:04 +0100

    adding login, register forms and main page restrustructure

---
+++ src/lib/components/auth/LoginForm.svelte
@@ -0,0 +1,64 @@
+<script>
+    let username = '';
+    let password = '';
+  
+    const handleLogin = async () => {
+    };
+  </script>
+
+<div class="login-container">
+    <h2>Please login</h2>
+  
+    <form action="">
+        <div class="input-group">
+            <label for="username">Email:</label>
+            <input type="email" bind:value={username} id="username" />
+        </div>
+        
+        <div class="input-group">
+            <label for="password">Password:</label>
+            <input type="password" bind:value={password} id="password" />
+        </div>
+        <input type="submit" on:click={handleLogin} class="login-button" value="Login" />
+    </form>
+  
+</div>
+  
+<style>
+    .login-container {
+        max-width: 500px;
+        padding: 20px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+  
+    .input-group {
+        margin-bottom: 15px;
+        width: 100%;
+    }
+  
+    .input-group label {
+        display: block;
+        margin-bottom: 5px;
+    }
+  
+    .input-group input {
+        width: 100%;
+        padding: 8px;
+        box-sizing: border-box;
+    }
+  
+    .login-button {
+        background-color: #4caf50;
+        color: white;
+        padding: 10px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+</style>
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
-<script>
+<script lang="ts">
+    import LoginForm from "$lib/components/auth/LoginForm.svelte";
+    import RegisterForm from "$lib/components/auth/RegisterForm.svelte";
+    let showLogin = false;
+    let showRegister = false;
 </script>
 
 <svelte:head>
@@ -6,27 +10,89 @@
     <meta name="description" content="Svelte demo app"/>    
 </svelte:head>
 
-<main>
-    <a href="/userspace">Show demo</a>
-    <a href="about">About</a>
-</main>
+<div class="nav">
+    <div class="left-section">
+        <a class="title" href="/">Online cashier system</a>
+    </div>
+    <div class="right-section">
+        <a class="tab" href="/userspace">Show demo</a>
+        <a class="tab" href="/about">About</a>
+        <button class="tab" on:click={() => {showLogin = true;}}>Login</button>
+        <button class="tab" on:click={() => {showRegister = true;}}>Register</button>
+    </div>
+</div>
+
+{#if showLogin}
+    <button class="overlay" on:click={() => {showLogin = false;}}></button>
+    <div class="form">
+        <LoginForm />
+    </div>
+{/if}
+{#if showRegister}
+    <button class="overlay" on:click={() => {showRegister = false;}}></button>
+    <div class="form">
+        <RegisterForm />
+    </div>
+{/if}
 
 <style>
-    main {
+    .nav {
         display: flex;
-        flex-direction: column;
         align-items: center;
-        position: fixed;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
+        background-color: #fff;
+        width: 100%;
     }
-    main a {
+
+    .left-section {
+        display: flex;
+        margin-left: 15px;
+    }
+
+    .tab {
         text-decoration: none;
-        font-size: 50px;
+        font-size: 20px;
+        cursor: pointer;
+        border: none;
+        background-color: #fff;
+        padding: 5px;
     }
-    main a:hover {
+
+    .tab:hover {
         color: blue;
         transition-duration: 1s;
+    }
+
+    .title {
+        text-decoration: none;
+        font-size: 25px;
+    }
+
+    .right-section {
+        margin-left: auto;
+        margin-right: 15px;
+    }
+
+    .form {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        height: 90%;
+        width: 70%;
+        background-color: #fff;
+        box-shadow: -2px 0 5px rgba(0, 0, 0, 0.1);
+        overflow-y: auto;
+        z-index: 1000;
+        border-radius: 10px;
+    }
+
+    .overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(186, 186, 186, 0.5);
+        z-index: 999;
     }
 </style>
